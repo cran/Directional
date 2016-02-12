5a4c8a15106da0d9204a825e157b5f3a45055799
Michail Tsagris <mtsagris@yahoo.gr>
2016-02-12 14:40:59 +0000

    version 1.7.5

--- R/vmkde.tune_2.R
+++ R/vmkde.tune_2.R
@@ -13,13 +13,13 @@ vmkde.tune_2 <- function(u, low = 0.1, up = 1, rads = TRUE) {
   if (rads == FALSE)  u <- u/180 * pi  
   disa <- as.matrix(dist(u))
   funa <- function(h) { 
-  A <- exp(cos(disa)/h^2)
+  A <- exp( cos(disa) / h^2 )
     diag(A) <- NA  ## we do not want the diagonal elements
-    f <- rowSums(A, na.rm = T)/((n - 1) * 2 * pi * besselI(1/h^2, 0))
-    mean(log(f))
+    f <- rowSums( A, na.rm = T )/( (n - 1) * 2 * pi * besselI(1/h^2, 0) )
+    mean( log(f) )
     }
   bar <- optimize(funa, c(low, up), maximum = T) 
-  res <- c(bar$maximum, bar$objective)
+  res <- c( bar$maximum, bar$objective )
   names(res) <- c("Optimal h", "cv")
   res 
 }
