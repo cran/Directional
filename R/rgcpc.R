1dcff74e69fb791b123b029a877b6aa8d0a1916e
Michail Tsagris <mtsagris@uoc.gr>
2023-04-03 07:20:06 +0000

    version 6.0

--- R/rgcpc.R
+++ R/rgcpc.R
@@ -1,7 +1,7 @@
 rgcpc <- function(n, mu = NULL, omega, g, rho, rads = TRUE) {
   if ( is.null(mu) ) {
     if ( !rads )  omega <- omega * pi/180
-    ksi <- cbind( cos(mu), sin(mu) )
+    ksi <- cbind( cos(omega), sin(omega) )
     mu <- g * ksi
   } else  ksi <- mu / sqrt( sum(mu^2) )
   sinv <- matrix( c( ksi[1]^2 + ksi[2]^2/rho, ksi[1] * ksi[2] * (1 - 1/rho), 
